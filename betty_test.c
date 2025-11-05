e2e001f5cf7c2b0e9cc4665ed010023187060fcc
skicodes <skalrashed02@hotmail.com>
2025-11-05 16:29:46 +0000

    Task 1: code passes Betty checks

---
+++ betty_test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Hello, Betty!" to the console.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Hello, Betty!\n");
+    return (0);
+}
