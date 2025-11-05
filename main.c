a66344d0c3e43f136b9898200933fb7bbc22d8e9
skicodes <skalrashed02@hotmail.com>
2025-11-05 17:01:14 +0000

    Fix: Simple shell 0.1 execve call to use argv array instead of NULL

--- main.c
+++ main.c
@@ -2,38 +2,35 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <ctype.h>
 
 /**
- * trim_spaces - remove leading and trailing spaces
+ * trim_spaces - removes leading and trailing spaces from a string
  * @str: string to trim
  * Return: pointer to trimmed string
  */
 char *trim_spaces(char *str)
 {
-    char *end;
+	char *end;
 
-    if (str == NULL)
-        return (NULL);
+	while (isspace((unsigned char)*str))
+		str++;
 
-    while (isspace((unsigned char)*str))  /* remove leading spaces */
-        str++;
+	if (*str == 0)
+		return str;
 
-    if (*str == 0)  /* all spaces */
-        return (str);
+	end = str + strlen(str) - 1;
+	while (end > str && isspace((unsigned char)*end))
+		end--;
 
-    end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end))
-        end--;
+	end[1] = '\0';
 
-    *(end + 1) = '\0';
-    return (str);
+	return str;
 }
 
 /**
- * main - simple shell 0.1
+ * main - simple shell
  * @argc: argument count
  * @argv: argument vector
  * @envp: environment variables
@@ -41,52 +38,61 @@ char *trim_spaces(char *str)
  */
 int main(int argc, char **argv, char **envp)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    pid_t pid;
-    int status;
-    char *cmd;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
+	int status;
+	char *cmd;
 
-    (void)argc;
-    (void)argv;
+	(void)argc;
 
-    while (1)
-    {
-        write(1, "($) ", 4);
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)  /* handle EOF */
-        {
-            write(1, "\n", 1);
-            break;
-        }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(1, "$ ", 2);
 
-        line[nread - 1] = '\0';  /* remove newline */
-        cmd = trim_spaces(line);
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+			break;
 
-        if (cmd[0] == '\0')  /* empty line */
-            continue;
+		cmd = trim_spaces(line);
+		if (cmd[0] == '\0')
+			continue;
 
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            continue;
-        }
-        if (pid == 0)  /* child process */
-        {
-            execve(cmd, NULL, envp);
-            /* if execve fails */
-            write(2, argv[0], strlen(argv[0]));
-            write(2, ": 1: ", 5);
-            write(2, cmd, strlen(cmd));
-            write(2, ": not found\n", 12);
-            exit(EXIT_FAILURE);
-        }
-        else  /* parent process */
-            waitpid(pid, &status, 0);
-    }
+		if (strcmp(cmd, "exit") == 0)
+			break;
 
-    free(line);
-    return (0);
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			continue;
+		}
+		if (pid == 0)
+		{
+			/* Child process */
+			char *argv_exec[2];
+
+			argv_exec[0] = cmd;
+			argv_exec[1] = NULL;
+
+			execve(cmd, argv_exec, envp);
+
+			/* execve failed */
+			write(2, argv[0], strlen(argv[0]));
+			write(2, ": 1: ", 5);
+			write(2, cmd, strlen(cmd));
+			write(2, ": not found\n", 12);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			/* Parent process */
+			wait(&status);
+		}
+	}
+
+	free(line);
+	return 0;
 }
