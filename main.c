d8bb3c6cf47d589ed24a8f8f93bb122572022fb2
skicodes <skalrashed02@hotmail.com>
2025-11-05 17:08:30 +0000

    Fix shell: C90 compliance, Betty style, remove warnings/errors

--- main.c
+++ main.c
@@ -2,97 +2,131 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <ctype.h>
 
 /**
- * trim_spaces - removes leading and trailing spaces from a string
- * @str: string to trim
+ * trim_spaces - remove leading and trailing spaces from a string
+ * @str: the string to trim
+ *
  * Return: pointer to trimmed string
  */
 char *trim_spaces(char *str)
 {
 	char *end;
 
+	if (str == NULL)
+		return (NULL);
+
+	/* Remove leading spaces */
 	while (isspace((unsigned char)*str))
 		str++;
 
-	if (*str == 0)
-		return str;
+	if (*str == '\0')
+		return (str);
 
+	/* Remove trailing spaces */
 	end = str + strlen(str) - 1;
 	while (end > str && isspace((unsigned char)*end))
 		end--;
 
-	end[1] = '\0';
-
-	return str;
+	*(end + 1) = '\0';
+	return (str);
 }
 
 /**
- * main - simple shell
- * @argc: argument count
- * @argv: argument vector
+ * execute_command - fork and execute a command
+ * @cmd: command to execute
  * @envp: environment variables
- * Return: 0 on success
+ *
+ * Return: 0 on success, 1 on failure
  */
-int main(int argc, char **argv, char **envp)
+int execute_command(char *cmd, char **envp)
+{
+	pid_t pid;
+	int status;
+	char *args[2];
+
+	if (cmd == NULL || *cmd == '\0')
+		return (1);
+
+	args[0] = cmd;
+	args[1] = NULL;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (1);
+	}
+	if (pid == 0)
+	{
+		execve(cmd, args, envp);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
+	return (0);
+}
+
+/**
+ * read_line - read a line from stdin
+ *
+ * Return: pointer to the line, caller must free it
+ */
+char *read_line(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
-	pid_t pid;
-	int status;
+
+	nread = getline(&line, &len, stdin);
+	if (nread == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	/* Remove trailing newline */
+	if (line[nread - 1] == '\n')
+		line[nread - 1] = '\0';
+
+	return (line);
+}
+
+/**
+ * main - simple shell main loop
+ * @argc: argument count
+ * @argv: argument vector
+ * @envp: environment variables
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv, char **envp)
+{
+	char *line;
 	char *cmd;
 
 	(void)argc;
+	(void)argv;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			write(1, "$ ", 2);
-
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
+		write(1, "$ ", 2);
+		line = read_line();
+		if (line == NULL)
 			break;
 
 		cmd = trim_spaces(line);
-		if (cmd[0] == '\0')
-			continue;
+		if (*cmd != '\0')
+			execute_command(cmd, envp);
 
-		if (strcmp(cmd, "exit") == 0)
-			break;
-
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			continue;
-		}
-		if (pid == 0)
-		{
-			/* Child process */
-			char *argv_exec[2];
-
-			argv_exec[0] = cmd;
-			argv_exec[1] = NULL;
-
-			execve(cmd, argv_exec, envp);
-
-			/* execve failed */
-			write(2, argv[0], strlen(argv[0]));
-			write(2, ": 1: ", 5);
-			write(2, cmd, strlen(cmd));
-			write(2, ": not found\n", 12);
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			/* Parent process */
-			wait(&status);
-		}
+		free(line);
 	}
 
-	free(line);
-	return 0;
+	write(1, "\n", 1);
+	return (0);
 }
