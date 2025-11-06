56f11097d2a6a288606b339e99efc5b2b0319625
skicodes <skalrashed02@hotmail.com>
2025-11-06 12:19:41 +0000

    Simple shell 0.1 - basic command line interpreter

---
+++ shell.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+#define PROMPT "#cisfun$ "
+
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    int status;
+
+    while (1)
+    {
+        /* Display prompt only if input is from terminal */
+        if (isatty(STDIN_FILENO))
+            printf("%s", PROMPT);
+
+        nread = getline(&line, &len, stdin);
+        if (nread == -1)
+        {
+            /* Handle Ctrl+D (EOF) */
+            if (isatty(STDIN_FILENO))
+                printf("\n");
+            break;
+        }
+
+        /* Remove trailing newline */
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
+
+        /* Ignore empty input */
+        if (line[0] == '\0')
+            continue;
+
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("Error:");
+            continue;
+        }
+
+        if (pid == 0)
+        {
+            /* Child process: try to execute the command directly */
+            char *argv[] = {line, NULL};
+
+            if (execve(line, argv, environ) == -1)
+            {
+                perror("./shell");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            /* Parent waits for child */
+            waitpid(pid, &status, 0);
+        }
+    }
+
+    free(line);
+    return 0;
+}
